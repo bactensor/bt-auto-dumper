22e796ca15b8bd6412653274d143014c76d2cf79
Hiroyuki <hiroyukikumazawa.jp@gmail.com>
2024-09-24 16:59:51 +0200

    fix: refactor cli to handle configuration updates and loading

--- src/bt_auto_dumper/_v2/__main__.py
+++ src/bt_auto_dumper/_v2/__main__.py
@@ -20,6 +20,8 @@ def main(apiver: str | None = None):
     apiver = apiver or pathlib.Path(__file__).parent.name
     parser = argparse.ArgumentParser(description=f"BT Auto Dumper CLI {apiver}")
     parser.add_argument("--note", help="Comment or note for the operation", type=str, default="")
+    parser.add_argument("--set-autovalidator-address", help="Set a new autovalidator address", type=str)
+    parser.add_argument("--set-codename", help="Set a new Subnet Identifier codename", type=str)
 
     args = parser.parse_args()
 
@@ -35,23 +37,16 @@ def main(apiver: str | None = None):
     # Define the full path for the configuration file
     config_path = os.path.join(config_expanded_dir, "config.ini")
 
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(f"{config_path} does not exist.")
+    # Check if the user wants to update config values
+    if args.set_autovalidator_address or args.set_codename:
+        update_confg(
+            config_path=config_path,
+            new_autovalidator_address=args.set_autovalidator_address,
+            new_codename=args.set_codename,
+        )
+        print(f"Configuration updated successfully at {config_path}")
 
-    # Read the configuration file
-    config = configparser.ConfigParser()
-    try:
-        config.read(config_path)
-    except Exception as e:
-        raise RuntimeError(f"Error reading configuration file: {config_path} \n Error:{e}")
-    print(config_path)
-    # Extract required values from the configuration
-    try:
-        autovalidator_address = config.get("subnet", "autovalidator_address")
-        subnet_identifier = config.get("subnet", "codename")
-    except Exception as e:
-        raise KeyError(f"Configuration error: Missing in the config file. \n Error:{e}")
-
+    autovalidator_address, subnet_identifier = load_config(config_path=config_path)
     dump_and_upload(subnet_identifier, autovalidator_address, args.note)
 
 
@@ -162,6 +157,77 @@ def send_to_autovalidator(
     else:
         print(f"Failed to upload file. Status code: {response.status_code}")
         print(response.text)
+
+
+def load_config(config_path: str) -> tuple[str, str]:
+    """
+    Load the configuration from the config file.
+
+    Args:
+        config_path (str): The path to the configuration file.
+
+    Returns:
+        tuple: A tuple containing the autovalidator address and the subnet codename.
+
+    """
+
+    # Check if the configration file exist
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"{config_path} does not exist.")
+
+    # Read the configuration file
+    config = configparser.ConfigParser()
+    try:
+        config.read(config_path)
+    except Exception as e:
+        raise RuntimeError(f"Error reading configuration file: {config_path} \n Error:{e}")
+
+    # Extract required values from the configuration
+    try:
+        autovalidator_address = config.get("autovalidator", "autovalidator_address")
+        subnet_identifier = config.get("autovalidator", "codename")
+    except Exception as e:
+        raise KeyError(f"Configuration error: Missing in the config file. \n Error:{e}")
+
+    return autovalidator_address, subnet_identifier
+
+
+def update_confg(config_path: str, new_autovalidator_address: str, new_codename: str):
+    """
+    Updates the configration with a new autovalidator address or codename.
+    If the config file doesn't exist, it creates a new one with the provided
+    new_autovalidator_address and new_codename.
+
+    Args:
+        config_path (str): The path to the configration file.
+        new_autovalidator_address (str): The new autovalidator address to be set.
+        new_codename (str): The new subnet identifier codename to be set.
+
+    """
+    # Initialize a ConfigParser object
+    config = configparser.ConfigParser()
+
+    # Check if the configuration file exists
+    if not os.path.exists(config_path):
+        config["autovalidator"] = {}
+    else:
+        try:
+            config.read(config_path)
+        except Exception as e:
+            raise RuntimeError(f"Error reading configuration file: {config_path} \n Error:{e}")
+
+    if new_autovalidator_address:
+        config.set("autovalidator", "autovalidator_address", new_autovalidator_address)
+
+    if new_codename:
+        config.set("autovalidator", "codename", new_codename)
+
+    # Write or update the configuration file.
+    try:
+        with open(config_path, "w") as configfile:
+            config.write(configfile)
+    except Exception as e:
+        raise RuntimeError(f"Failed to write to the configuration file: {config_path}.\n Error: {e}")
 
 
 if __name__ == "__main__":
